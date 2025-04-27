f99e2793759c614f00e549bc88a9047f527072d6
sehamanter1 <sehamanter2@gmail.com>
2025-04-28 02:39:15 +0300

    intial - base structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+     {console.log("first", process.env.BASE_URL)}   
         <Image
           className="dark:invert"
           src="/next.svg"
@@ -12,6 +14,11 @@ export default function Home() {
           height={38}
           priority
         />
+        <Link href="/about">
+        About
+</Link>
+ 
+
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
