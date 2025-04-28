2b027270246a6cc91bf475c1f129396d9ebc48d4
sehamanter1 <sehamanter2@gmail.com>
2025-04-28 18:18:46 +0300

    create axiosInstance service

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,10 +6,10 @@ export default function Hero() {
   return (
       <section className="flex flex-col-reverse md:flex-row items-center justify-between px-8 ">
           <div className="md:w-1/2 space-y-6">
-              <h1 className="text-4xl font-bold leading-snug">
+              <h1 className="text-4xl text-secondary font-bold leading-snug">
                   Make changes and <br /> help the world
               </h1>
-              <p className="text-gray-600">
+              <p className="text-secondary">
                   Hope for Tomorrow non-profit organization that collaborates with volunteers to deliver humanitarian
                   aid and disaster relief to vulnerable communities.
               </p>
