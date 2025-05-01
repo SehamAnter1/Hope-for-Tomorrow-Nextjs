8a9d653487f6cb600bd73e86443ff19cf7b6d4c6
sehamanter1 <sehamanter2@gmail.com>
2025-05-02 01:26:47 +0300

    update

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -4,7 +4,6 @@ import Stipe_Payment from "@/services/Pay_With_Stripe_Button";
 export default function Home() {
     return (
         <div className="flex flex-col md:flex-row">
-            <Stipe_Payment />
             <Hero />
         </div>
     );
