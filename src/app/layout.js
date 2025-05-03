e52b163749952c8166988b3edffff1193b2d0d37
sehamanter1 <sehamanter2@gmail.com>
2025-05-03 06:16:04 +0300

    add favicon and title

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,11 @@ import 'primereact/resources/primereact.min.css';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+    <head>
+    <link rel="icon" href="/logo.png" />
+    <title>Hope for Tomorrow</title>
+
+    </head>
       <body className="bg-gray-50 min-h-screen flex flex-col">
        {children}
       </body>
